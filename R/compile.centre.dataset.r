bb1118a443970960866f78f3b99dc65b981471ea
Martin Gerdin Wärnberg <martingerdin@gmail.com>
2019-03-06 23:36:18 +0100

    Add package references

--- R/compile.centre.dataset.r
+++ R/compile.centre.dataset.r
@@ -15,7 +15,7 @@ compile.centre.dataset <- function(
     files <- list.files(dir_path, full.names = TRUE, recursive = FALSE, pattern = ".txt$")
 ## ** import records
     if (length(files) > 0) {
-        dataset <- rbind.fill(lapply(files, import.record))
+        dataset <- plyr::rbind.fill(lapply(files, import.record))
     } else {
         dataset <- NULL
     }
--- R/import.record.r
+++ R/import.record.r
@@ -10,9 +10,9 @@ import.record <- function(
                           )
 {
 ## ** get record
-    text_record <- readtext(file_path)$text
+    text_record <- readtext::readtext(file_path)$text
 ## ** create record from JSON string
-    json <- tryCatch(fromJSON(text_record),
+    json <- tryCatch(jsonlite::fromJSON(text_record),
                      warning = function(w) {
                          print(w)
                          print(file_path)
@@ -22,7 +22,7 @@ import.record <- function(
                          print(file_path)
                          print(e)
                          preprocessed <- preprocess.record(text_record)
-                         json <- fromJSON(preprocessed)
+                         json <- jsonlite::fromJSON(preprocessed)
                          return(json)
                      })
     record <- as.data.frame(json, stringsAsFactors = FALSE)
